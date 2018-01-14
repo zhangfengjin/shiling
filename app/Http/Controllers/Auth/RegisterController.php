e2eb7bbfdd4c1298c0f45c8d83a0e9f5d65245c8
zhangfengjin <793665131>
2018-01-14 21:14:52 +0800

    register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Services\UserService;
 use App\User;
 use App\Http\Controllers\Controller;
+use App\Utils\DataStandard;
+use App\Utils\RegHelper;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -57,6 +62,46 @@ class RegisterController extends Controller
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function username()
+    {
+        return "tel";
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
+    public function register(Request $request)
+    {
+        $tel = $request->input('tel');
+        $email = $request->input('email');
+        $userService = new UserService ();
+        if (RegHelper::validateTel($tel)) {
+            if ($userService->uniqueTel($tel) > 0) {
+                return DataStandard::getStandardData([], "手机已被抢注", 203);
+            }
+        }
+        if (RegHelper::validateEmail($email)) {
+            if ($userService->uniqueEmail($email) > 0) {
+                return DataStandard::getStandardData([], "邮箱已被抢注", 204);
+            }
+        }
+        $user = $request->all();
+        $user = $userService->create($user); // 注册
+        if ($user) {
+            $this->guard()->login($user); // 登录
+            return DataStandard::getStandardData();
+        }
+        return DataStandard::getStandardData([], "注册失败", 204);
     }
 
     /**
--- app/Http/Services/UserService.php
+++ app/Http/Services/UserService.php
@@ -14,13 +14,52 @@ use App\User;
 class UserService
 {
     /**
+     * 判断email是否存在
+     *
+     * @param unknown $email
+     */
+    public function uniqueEmail($email)
+    {
+        return User::where("email", '=', $email)->count();
+    }
+
+    /**
+     * 判断电话号码是否存在
+     *
+     * @param unknown $tel
+     */
+    public function uniqueTel($tel)
+    {
+        return User::where("tel", '=', $tel)->count();
+    }
+
+
+    /**
+     * 重置密码
+     *
+     * @param unknown $account
+     * @param unknown $reqaccount
+     * @param unknown $password
+     * @return unknown
+     */
+    public function resetPwd($account, $reqaccount, $password)
+    {
+        $user = User::where($account, '=', $reqaccount)->first();
+        $user->password = bcrypt($password);
+        $user->save();
+        return $user;
+    }
+
+    /**
      * 添加用户
      * @param $input
      */
-    public function add($input)
+    public function create($input)
     {
         $user = new User();
-        $user->tel = 15510239712;
+        $user->tel = $input["tel"];
+        $user->password = $input["password"];
         $user->save();
+        return $user;
     }
 }
