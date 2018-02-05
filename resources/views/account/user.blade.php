76885443d6e0cd4d90c5ebd365d19469216e5bb1
zhangfengjin <793665131>
2018-02-05 14:48:13 +0800

    user stop

--- resources/views/account/user.blade.php
+++ resources/views/account/user.blade.php
@@ -303,11 +303,11 @@
                                 "info": "删除", "func": me._del
                             },
                             /*"pass": {
-                                "info": "批量通过", "func": me._egis
-                            },
-                            "refuse": {
-                                "info": "批量驳回", "func": me._refuse
-                            },*/
+                             "info": "批量通过", "func": me._egis
+                             },
+                             "refuse": {
+                             "info": "批量驳回", "func": me._refuse
+                             },*/
                             "export": {
                                 "info": "导出", "func": function (ids) {
                                     location.href = CommonUtil.getRootPath() + userUrl + "/export?searchs=" + JSON.stringify(searchInfo ? searchInfo["searchs"] : "");
@@ -354,7 +354,7 @@
                                                         if (text != "") {
                                                             TableList.optTable({
                                                                 "tableId": tableId,
-                                                                "url": userUrl + "/stop/" + ids,
+                                                                "url": userUrl + "/stop/" + ids + "?code=" + text,
                                                                 "type": "DELETE",
                                                                 "async": true,
                                                                 "successfn": function () {
